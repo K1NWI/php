92aefbca4417ceb51f4353568e64274b873c7ace
K1NWI <130763672+K1NWI@users.noreply.github.com>
2023-11-09 11:15:53 +0100

    z

--- users.php
+++ users.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "1234", "Trabajadores2");
+$conn = new mysqli("localhost", "root", "", "Trabajadores2");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
