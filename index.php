f2243e8299f0e3f2bbd6482626716f783dd8838f
K1NWI <130763672+K1NWI@users.noreply.github.com>
2023-11-20 10:57:16 +0100

    a

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
-<?php 
-include('users.php');
-$sql = "SELECT * FROM usuarios";
-$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola</title>
+    <title>Tabla de usuarios</title>
 </head>
 <body>
-    <table align="center" border="5px" style="width:80%; line-height:30px">
-        <th colspan="3"><h2>Usuarios</h2></th>
-        <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Correo</th>
-        </tr>
-        <?php while ($row = mysqli_fetch_array($result)) {?>
-            <tr>
-                <td><?php echo $row['nombre'];?></td>
-                <td><?php echo $row['apellido'];?></td>
-                <td><?php echo $row['correo'];?></td>
-            </tr>  
-        <?php }?>
-    </table>
+<?php
+// Datos de conexión a la base de datos
+$host = "localhost"; // Cambia a la dirección de tu servidor MySQL
+$usuario = "kini";
+$contrasena = "1234";
+$base_de_datos = "Trabajadores2";
+// Conexión a la base de datos
+$conn = new mysqli($host, $usuario, $contrasena, $base_de_datos);
+// Verificar la conexión
+if (!$conn) {
+    die("Error en la conexión: " . mysqli_connect_error());
+}
+// Consulta SQL para obtener datos de la tabla
+$sql = "SELECT * FROM nombre_de_la_tabla"; // Cambia 'nombre_de_la_tabla' al nombre de tu tabla
+$result = mysqli_query($conn, $sql);
+// Procesar y mostrar los datos
+if (mysqli_num_rows($result) > 0) {
+    while ($fila = mysqli_fetch_assoc($result)) {
+        // Aquí puedes mostrar o procesar los datos como desees
+        echo "Nombre: " . $fila["nombre"] . "<br>";
+        echo "Correo: " . $fila["correo"] . "<br>";
+        // Puedes agregar más campos según tu tabla
+    }
+} else {
+    echo "No se encontraron resultados en la tabla.";
+}
 
-    <?php
-    // Cierra la conexión después de usarla
-    mysqli_close($conn);
-    ?>
+// Cerrar la conexión
+mysqli_close($conn);
+?>   
+
+ <!-- Puedes continuar con más contenido HTML -->
 </body>
 </html>
